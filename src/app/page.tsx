659f9ef061511e847a23f35808163fa5b8bf6656
Aidan Lee <aidanjlee5@gmail.com>
2024-04-23 00:22:21 -0400

    Add more functionality to the dashboard, shoutout to Anisha for makign this whole thing possible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,19 +33,19 @@ export default function Home() {
             <div className="bg-yellow-200 w-[20%] py-4 px-10 rounded-t-lg text-xl">Focused Topic</div>
             <section>
               <h1 className='text-2xl font-bold'>Why you should rent, even if you’re ‘rich’</h1>
-              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
+              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
             </section>
             <section>
               <h1 className='text-2xl font-bold'>Why you should rent, even if you’re ‘rich’</h1>
-              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
+              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
             </section>
             <section>
               <h1 className='text-2xl font-bold'>Why you should rent, even if you’re ‘rich’</h1>
-              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
+              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
             </section>
             <section>
               <h1 className='text-2xl font-bold'>Why you should rent, even if you’re ‘rich’</h1>
-              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
+              <p className='mt-2 text-lg'>Well, according to the host of Netflix’s “How To Get Rich” and the very dude who founded this newsletter, the answer in an economy this ugly is to rent without remorse (and invest). Hear Ramit’s napkin math and Sam’s tips for home hunters on our top-ranked podcast, My First Million.</p>
             </section>
           </div>
           <div className="flex flex-col">
@@ -56,7 +56,7 @@ export default function Home() {
               <div className="bg-orange-200 py-4 px-10 rounded-t-lg text-lg">Topic</div> 
               <div className="bg-purple-200 py-4 px-10 rounded-t-lg text-lg">Topic</div> 
             </div>
-            
+            <div className='h-16 bg-yellow-200'></div>
           </div>
         </div>
       <div className="w-full">
@@ -64,11 +64,22 @@ export default function Home() {
           <div className="flex w-full h-full col-span-1">
               <div className='w-full ml-8 flex flex-col mt-24' style={{ width: '60%' }}>
                 <div className='flex flex-col gap-y-2'>
-                  
+                  <div className='rounded-lg bg-yellow-200 py-4 mb-2 px-4 text-xl'>For You</div>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                </div>
+                <div className='flex flex-col gap-y-2 mt-8'>
+                  <div className='rounded-lg bg-yellow-200 mb-2 py-4 px-4 text-xl'>Trending Global</div>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
+                  <p className='text-lg '><b>Glencore's</b> emissions rose ~9% in 2023</p>
                 </div>
             </div>
             <a href="/settings" className="fixed bottom-0 left-0 p-4">
-              <FontAwesomeIcon icon={faGear} color='#000000' width='70' className='size-8 fixed top-3 right-3 p-4 hover:animate-spin-slow'/>
+              <FontAwesomeIcon icon={faGear} color='#000000' className='size-10 fixed top-3 right-3 p-4'/>
             </a>
           </div>
         </div>
@@ -133,8 +144,20 @@ export default function Home() {
               <div className='h-16 bg-yellow-200'></div>
             </div>
           </div>
-        </div>
+          <div className='bg-gradient-to-b from-yellow-200 to-white h-[1200px] w-full justify-end'></div>
+          <div className="snap-center h-screen w-screen flex flex-col">
+            <div className='flex flex-col w-[85%] items-center justify-center h-screen'>
+              <Image 
+                src="/check.png" 
+                alt="Check Image" 
+                width={250} 
+                height={250} 
+              />
+              <p className='text-2xl text-center pt-8'>You’ve finished your brief for the day. <br/>Check back tomorrow for more info or <a href='#' className='underline'>restart.</a></p>
+            </div>
+          </div>
       </div>
     </div>
+  </div>
   );
 }
